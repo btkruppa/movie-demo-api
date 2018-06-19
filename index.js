4ed76832bae5089903531e1c4f23cd3470afdd33
Blake Kruppa <btkruppa513@gmail.com>
2018-06-19 15:22:03 -0400

    added an endpoint

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 var fs = require('fs');
 var path = require('path');
+const aws = require('aws-sdk');
+const docClient = new aws.DynamoDB.DocumentClient();
 
 exports.get = function(event, context, callback) {
   var contents = fs.readFileSync(`public${path.sep}index.html`);
@@ -13,3 +15,45 @@ exports.get = function(event, context, callback) {
 
   callback(null, result);
 };
+
+exports.findByYear = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify([{
+      title: 'Test movie',
+      year: 2018
+    }]),
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    }
+  })
+    // findAllByYear(parseInt(event.pathParameters.year, 10))
+    //     .then(data => {
+    //         callback(null, {
+    //             statusCode: 200,
+    //             body: JSON.stringify(data.Items),
+    //             headers: {
+    //               'Access-Control-Allow-Origin': '*'
+    //             }
+    //         });
+    //     })
+    //     .catch(err => {
+    //         callback(err);
+    //     });
+    
+};
+
+function findAllByYear(year) {
+  console.log(`Querying for movies from ${year}.`);
+
+  return docClient.query({
+    TableName: 'movies',
+    KeyConditionExpression: '#yr = :yyyy',
+    ExpressionAttributeNames: {
+      '#yr': 'year'
+    },
+    ExpressionAttributeValues: {
+      ':yyyy': year
+    }
+  }).promise(); // Most of the time we will want to do it this way
+}
